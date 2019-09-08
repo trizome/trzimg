c9d6b7ad672c6ffe85abf1d4267b9ac5f81902eb
myappliweb <myappliweb@gmail.com>
2019-09-08 16:41:44 +0200

    best src

--- src/Img.js
+++ src/Img.js
@@ -3,19 +3,17 @@ import React from 'react';
 export default class Img extends React.Component {
     
     render(){
-    const {src, width, height, opacity} = this.props
-    const img =src[ Math.floor(Math.random()*src.length)];
-    console.log(img)
-
+    const {src, width='100%', height='100%', opacity=1, filter = 'none'} = this.props
+    const img = Array.isArray(src) ? src[ Math.floor(Math.random()*src.length)]: src;
     const style={
         content : {
                 width,
                 height,
                 background: `url(${img}) no-repeat center center`,
                 backgroundSize: 'cover',
-                opacity
+                opacity,
+                filter
             },
-
     }
   return (
     <div style={style.content}>
